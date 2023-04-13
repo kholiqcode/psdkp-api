ac6ebe51219673411b8bad32a239e59db245e1d0
Kholiq <kholiqcode@gmail.com>
2023-04-13 07:41:48 +0700

    feat: create ship model

---
+++ app/Models/Ship.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ship extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'code',
+        'name',
+        'owner_name',
+        'owner_street_address',
+        'ship_size',
+        'photo_path',
+        'total_crew',
+        'permission_number',
+        'permission_document_path',
+        'user_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+}
